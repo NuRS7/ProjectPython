1803d3cbaabee2ff5af27f15f62f9ce0df747712
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-12 17:33:37 +0500

    Add new function to project audio file convert to text

--- BackEndVirtualChatAssistant/main.py
+++ BackEndVirtualChatAssistant/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai
 
-
+from functions.openai_requests import convert_audio_to_text
 app = FastAPI()
 
 @app.get("/chat")
@@ -12,39 +12,16 @@ async def chat_asisstant():
     return {"response":"chat"}
 
 
-@app.get("/reset")
-async def reset_conversation():
-    reset_messages()
-    return {"response": "conversation reset"}
+# @app.get("/reset")
+# async def reset_conversation():
+#     reset_messages()
+#     return {"response": "conversation reset"}
 
 
 @app.post("/post-audio/")
-async def post_audio(file:UploadFile = File(...)):
-    with open(file.filename, "wb") as buffer:
-        buffer.write(file.file.read())
-    audio_input = open(file.filename, "rb")
-
-    #Декод жасайауға фунция
+async def get_audio():
+    audio_input = open("voice.mp3","rb")
     message_decoded = convert_audio_to_text(audio_input)
+    print(message_decoded)
+    return "Done"
 
-    # если сообщения не докодировано вернем ошибку
-    if not message_decoded:
-        raise HTTPException(status_code=400, detail="Failed to decode audio")
-
-
-    chat_response=get_chat_response(message_decoded)
-
-    store_messages(message_decoded, chat_response)
-
-    if not chat_response:
-        raise HTTPException(status_code=400, detail="Failed chat response")
-
-    audio_output=convert_text_to_speech(chat_response)
-
-
-    if not audio_output:
-        raise HTTPException(status_code=400, detail="Failed audio output")
-    def iterfile():
-        yield audio_output
-
-    return StreamingResponse(iterfile(), media_type="application/octet-stream")
